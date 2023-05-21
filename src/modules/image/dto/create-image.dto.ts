ac8dbe67671678ca51ca067b7216cb1b74c816b2
moisesPompilio <moisesjtmed@gmail.com>
2023-05-20 23:59:12 -0300

    feat(Image):  criação do DTO a ser inserido para poder executar a regra de trabalho

---
+++ src/modules/image/dto/create-image.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, Max, IsPositive, Validate } from 'class-validator';
+import { IsUrlAccessible } from 'src/shared/decorators/is_url_accessible.decorator';
+import { IsCompress } from '../validator/compress.validator';
+
+export class CreateImageDto {
+  @ApiProperty({
+    description: 'Link of the image',
+    example:
+      'https://assets.storage.trakto.io/AkpvCuxXGMf3npYXajyEZ8A2APn2/0e406885-9d03-4c72-bd92-c6411fbe5c49.jpeg',
+  })
+  @IsString()
+  // @Validate(IsUrlAccessible)
+  @IsUrlAccessible()
+  image: string;
+
+  @IsPositive()
+  @IsNumber()
+  @Validate(IsCompress)
+  @ApiProperty({
+    description:
+      'The compression ratio of the image must be greater than 0 and less than or equal to 1.',
+    example: 0.9,
+  })
+  compress: number;
+}
