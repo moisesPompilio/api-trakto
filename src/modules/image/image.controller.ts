1bcfaa67b9b25c60ce9ba879c96a84dc8b4e5c40
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:38:24 -0300

    feat: adicionado descrição para a documentação com o swagger

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,11 +1,28 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { CreateImageDto } from './dto/create-image.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Image } from './entities/image.entity';
 
+@ApiTags('image')
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
+  @ApiOperation({
+    summary:
+      'Salvar imagem e salvar copia da imagem com redimesionamento dela de maneira correta e compactar conforme os dados passado',
+  })
+  @ApiResponse({
+    status: 201,
+    description:
+      'Salvar imagem correto e retorna dos paths das imagens e metadata da imagem original',
+    type: Image,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'erro interno ao fazer a operação',
+  })
   @Post()
   save(@Body() createImageDto: CreateImageDto) {
     return this.imageService.save(createImageDto);
