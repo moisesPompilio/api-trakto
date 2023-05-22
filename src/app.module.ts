ade6f41b1d9533e522a5e7c77efa75616297018e
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:34:33 -0300

    refactor: retirada do modulo do user que não precisar mais dele non projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ImageModule } from './modules/image/image.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './modules/user/user.module';
 import 'dotenv/config';
 
 @Module({
@@ -10,7 +9,6 @@ import 'dotenv/config';
       `${process.env.DB_CONECTOR}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authMechanism=DEFAULT&authSource=admin`,
     ),
     ImageModule,
-    UserModule,
   ],
 })
 export class AppModule {}
