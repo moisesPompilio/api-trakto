958c534aa9cc085335d88db357dc678f3808e7b6
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:41:10 -0300

    feat: criação de uma classe para fazer os caminhos da imagem origina e thum com nome unico

---
+++ src/modules/image/dto/paths-images.dto.ts
@@ -0,0 +1,18 @@
+import { imageConfigManager } from 'src/config/image/image-config-manager';
+import { v4 as uuidv4 } from 'uuid';
+
+export class PathsImages {
+  private _imagePathOriginal: string;
+  private _imagePathThumb: string;
+  constructor() {
+    const uniqueName = uuidv4();
+    this._imagePathOriginal = `${imageConfigManager.originalDirectory()}/${uniqueName}.jpg`;
+    this._imagePathThumb = `${imageConfigManager.thumbnailDirectory()}/${uniqueName}_thumb.jpg`;
+  }
+  public get imagePathOrigin(): string {
+    return this._imagePathOriginal;
+  }
+  public get imagePathThumb(): string {
+    return this._imagePathThumb;
+  }
+}
