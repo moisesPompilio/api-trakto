e1a0790c66c63f6616abad31417e89acf3d37a46
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:39:03 -0300

    feat: finalizando a regra negocio do service

--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,18 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateImageDto } from './dto/create-image.dto';
-import { downloadImage } from './util/downloadImage.util';
-import { compressImage } from './util/compressImage.util';
-import { resizeImage } from './util/resizeImage.util';
+import { downloadImage } from 'src/shared/util/download-image.util';
+import { compressAndResizeImage } from 'src/shared/util/compress-and-resize-Image.util';
+import { PathsImages } from './dto/paths-images.dto';
+import { imageConfigManager } from 'src/config/image/image-config-manager';
+import { extratMetadata } from 'src/shared/util/extrat-metadata.util';
+import { Image, ImageDocument } from './entities/image.entity';
+import { excludeFile } from 'src/shared/util/exclude-file.util';
 
 @Injectable()
 export class ImageService {
+  constructor(
+    @InjectModel(Image.name) private readonly imageModel: Model<ImageDocument>,
+  ) {}
+
   async save(createImageDto: CreateImageDto) {
-    const imagePathOrigin = './images/original/iamgesteste.jpg';
-    await downloadImage(createImageDto.image, imagePathOrigin);
-    const imagePathThumb = './images/thumb/iamgesteste_thumb.jpg';
-    const maxSize = 720;
-    await resizeImage(imagePathOrigin, imagePathThumb, maxSize);
-    await compressImage(imagePathThumb, createImageDto.compress);
-    return createImageDto;
+    const imagesPaths = new PathsImages();
+    try {
+      await downloadImage(createImageDto.image, imagesPaths.imagePathOrigin);
+      const metadata = await extratMetadata(imagesPaths.imagePathOrigin);
+      await compressAndResizeImage(
+        imagesPaths.imagePathOrigin,
+        imageConfigManager.maxDimension() < metadata.width
+          ? imageConfigManager.maxDimension()
+          : metadata.width,
+        imageConfigManager.maxDimension() < metadata.height
+          ? imageConfigManager.maxDimension()
+          : metadata.height,
+        createImageDto.compress,
+        imagesPaths.imagePathThumb,
+      );
+      const output: Image = {
+        localpath: {
+          original: imagesPaths.imagePathOrigin,
+          thumb: imagesPaths.imagePathThumb,
+        },
+        metadata,
+      };
+      const createImage = new this.imageModel(output);
+      await createImage.save();
+      return createImage;
+    } catch (error) {
+      excludeFile(imagesPaths.imagePathOrigin);
+      excludeFile(imagesPaths.imagePathThumb);
+      throw error;
+    }
   }
 }
