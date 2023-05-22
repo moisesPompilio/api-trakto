edabd44587843d9db26232d3957d0cc90aa52293
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:37:47 -0300

    feat: adicionamento do creater folder, para ja fazer a verificação se existe as pastas necessaria para o sistema funcionar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger';
-import * as cors from 'cors';
-import 'dotenv/config';
+import { createRequiredFolders } from './config/image/create-required-folders';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
+
+  createRequiredFolders();
 
   await app.listen(process.env.PORT);
 }
