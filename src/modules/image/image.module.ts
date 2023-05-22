eed89953748b38394a3b9301d05c656851cf5bac
moisesPompilio <moisesjtmed@gmail.com>
2023-05-21 23:39:34 -0300

    feat: adicionamento da configuação de schema para o mongosse

--- src/modules/image/image.module.ts
+++ src/modules/image/image.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Image, ImageSchema } from './entities/image.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Image.name, schema: ImageSchema }]),
+  ],
   controllers: [ImageController],
   providers: [ImageService],
 })
