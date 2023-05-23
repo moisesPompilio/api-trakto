6d442d6791750703d9f4b3eff2101a97467391d8
moisesPompilio <moisesjtmed@gmail.com>
2023-05-23 17:54:49 -0300

    refactor: da classe para melhor compressao do conteudo

--- src/shared/util/extrat-metadata.util.ts
+++ src/shared/util/extrat-metadata.util.ts
@@ -1,7 +1,7 @@
 import * as sharp from 'sharp';
 import { ExtractMetadataException } from '../exceptions/extrat-metadata.exception';
 
-export async function extratMetadata(path: string): Promise<sharp.Metadata> {
+export async function extractMetadata(path: string): Promise<sharp.Metadata> {
   try {
     return await sharp(path).metadata();
   } catch (error) {
