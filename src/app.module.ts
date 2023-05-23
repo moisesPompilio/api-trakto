55c643040c5dd9c4fe1ef7b301c7f21b9b99c0c4
moisesPompilio <moisesjtmed@gmail.com>
2023-05-23 00:08:30 -0300

    feat: integração do custom filter no sistema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ImageModule } from './modules/image/image.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import 'dotenv/config';
+import { APP_FILTER } from '@nestjs/core';
+import { CustomExceptionFilter } from './shared/exceptions/custom-exception-filter.exception';
 
 @Module({
   imports: [
@@ -9,6 +11,12 @@ import 'dotenv/config';
       `${process.env.DB_CONECTOR}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authMechanism=DEFAULT&authSource=admin`,
     ),
     ImageModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger';
 import { createRequiredFolders } from './config/image/create-required-folders';
+import { CustomExceptionFilter } from './shared/exceptions/custom-exception-filter.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalFilters(new CustomExceptionFilter());
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
