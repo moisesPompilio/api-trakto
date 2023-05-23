f15af8fd954db3f97dbdc59a9e1ee55342106ab5
moisesPompilio <moisesjtmed@gmail.com>
2023-05-23 19:32:06 -0300

    refact: melhoramento na descrição do swagger

--- src/modules/image/model/create-image.model.ts
+++ src/modules/image/model/create-image.model.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUrlWithCodeExcption } from 'src/shared/decorators/is-url.-with-code-exception.decorator';
 import { IsCompress } from '../../../shared/decorators/Is-compress.decorator';
+import { IsUrlWithCodeExcption } from 'src/shared/decorators/is-url.-with-code-exception.decorator';
 
 export class CreateImage {
   @ApiProperty({
-    description: 'Link of the image',
+    description:
+      'image link, which will be saved and have a compressed and resized copy',
     example:
       'https://assets.storage.trakto.io/AkpvCuxXGMf3npYXajyEZ8A2APn2/0e406885-9d03-4c72-bd92-c6411fbe5c49.jpeg',
   })
@@ -14,7 +15,7 @@ export class CreateImage {
   @IsCompress()
   @ApiProperty({
     description:
-      'The compression ratio of the image must be greater than 1 and less than or equal to 99.',
+      'Compress will be the compression factor of the image, this factor must be an integer between 1 and 100.',
     example: 15,
   })
   compress: number;
